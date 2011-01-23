490e91a070f2dfdc399ef0c7547b8257fb0f03d7
James Coglan <jcoglan@gmail.com>
2011-01-23 12:27:19 +0000

    Move some models.

--- lib/pop_trumps.rb
+++ lib/pop_trumps.rb
@@ -3,6 +3,6 @@ require 'active_record'
 module PopTrumps
   ROOT = File.expand_path(File.dirname(__FILE__))
   
-  autoload :Artist, ROOT + '/pop_trumps/artist'
-  autoload :User,   ROOT + '/pop_trumps/user'
+  autoload :Artist, ROOT + '/pop_trumps/model/artist'
+  autoload :User,   ROOT + '/pop_trumps/model/user'
 end
