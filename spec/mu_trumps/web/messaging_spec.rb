00c7d46523a8460a34da628e045463c0afcea42e
James Coglan <jcoglan@gmail.com>
2011-01-25 10:55:43 +0000

    Refactor Web::Messaging spec.

--- spec/mu_trumps/web/messaging_spec.rb
+++ spec/mu_trumps/web/messaging_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
+RSpec::Matchers.define :take do |duration|
+  match do |proc|
+    begin
+      start = Time.now
+      proc.call
+      diff = Time.now - start
+      
+      case duration
+      when Numeric then diff >= duration
+      when Range then diff >= duration.begin and diff < duration.end
+      end
+    rescue => e
+      false
+    end
+  end
+end
+
 describe MuTrumps::Web::Messaging do
   let(:app) do
     app = MuTrumps::Web::Messaging.new(:timeout => 2)
@@ -10,9 +27,9 @@ describe MuTrumps::Web::Messaging do
   before(:all) { app.start(8000) }
   after(:all)  { app.stop }
   
-  def get(path)
-    uri = URI.parse('http://localhost:8000' + path)
-    Net::HTTP.get_response(uri)
+  def connect
+    uri = URI.parse("http://localhost:8000/alice")
+    Net::HTTP.get_response(uri).body
   end
   
   let(:alice) { Factory :user, :lastfm_username => "alice" }
@@ -24,18 +41,27 @@ describe MuTrumps::Web::Messaging do
     game.join(bob)
   end
   
-  it "receives no events after a timeout" do
-    time = Time.now
-    response = get "/alice"
-    response.body.should == "[]"
-    (Time.now - time).should >= 2
+  describe "when no messages are sent" do
+    it "receives no events" do
+      connect.should == "[]"
+    end
+    
+    it "returns after the timeout" do
+      lambda { connect }.should take(2)
+    end
   end
   
-  it "receives an event when it is published" do
-    EM.add_timer(1) { MuTrumps::Web::Messaging.publish(alice, "hello") }
-    time = Time.now
-    response = get "/alice"
-    response.body.should == '[{"event":"hello"}]'
-    (Time.now - time).should be_between(1,2)
+  describe "when a message is sent" do
+    before do
+      EM.add_timer(1) { MuTrumps::Web::Messaging.publish(alice, "hello") }
+    end
+    
+    it "receives an event" do
+      connect.should == '[{"event":"hello"}]'
+    end
+    
+    it "returns when the message is sent" do
+      lambda { connect }.should take(1..1.2)
+    end
   end
 end
